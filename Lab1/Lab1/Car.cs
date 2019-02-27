1866b5bf5c6455561b1b327bcf226f347e488c22
Rafal G <rafal.grycuk@gmail.com>
2019-02-27 17:19:21 +0100

    Lab1 - MASTER

---
+++ Lab1/Lab1/Car.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _doorCount;
+        private float _engineVolume;
+        private double _avgConsump;
+        private string _registrationNumber;
+
+        private static int _carCount = 0;
+
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set { _doorCount = value; }
+        }
+
+        public float EngineVolume
+        {
+            get { return _engineVolume; }
+            set { _engineVolume = value; }
+        }
+
+        public double AvgConsump
+        {
+            get { return _avgConsump; }
+            set { _avgConsump = value; }
+        }
+
+        public string RegistrationNumber
+        {
+            get => _registrationNumber;
+            set => _registrationNumber = value;
+        }
+
+        public Car()
+        {
+            _avgConsump = 0.0;
+            _brand = "none";
+            _doorCount = 0;
+            _engineVolume = 0.0F;
+            _model = "none";
+            _registrationNumber = "none";
+            _carCount++;
+        }
+
+        public Car(string brand, string model, int doorCount, float engineVolume, double avgConsump, string registrationNumber)
+        {
+            _brand = brand;
+            _model = model;
+            _doorCount = doorCount;
+            _engineVolume = engineVolume;
+            _avgConsump = avgConsump;
+            _registrationNumber = registrationNumber;
+            _carCount++;
+        }
+
+        public override string ToString()
+        {
+            return $"Car | Brand: {_brand}, Model: {_model}, NumOfDoors: {_doorCount}, EngineVol: {_engineVolume}, AvgConsump: {_avgConsump}, RegistrationNumber: {_registrationNumber}";
+        }
+
+        public double CalculateConsump(double roadLength)
+        {
+            return (_avgConsump * roadLength) / 100.0;
+        }
+
+        public double CalculateCost(double roadLength, double petrolCost)
+        {
+            return CalculateConsump(roadLength) * petrolCost;
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+        public static void DisplayCarCount()
+        {
+            Console.WriteLine($"Car instances count: {_carCount}");
+        }
+
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Details();
+            car1.Brand = "Fiat";
+            car1.Model = "126p";
+            car1.DoorCount = 2;
+            car1.EngineVolume = 650;
+            car1.AvgConsump = 6.0;
+            car1.RegistrationNumber = "KR12345";
+            car1.Details();
+            Car car2 = new Car("Syrena", "105", 2, 0.8f, 7.6d, "WE1234");
+            car2.Details();
+            Console.WriteLine(car1);
+            double routeConsumption = car2.CalculateConsump(500);
+            Console.WriteLine($"Route consumption: {routeConsumption} l");
+            double routeCost = car2.CalculateCost(500, 5);
+            Console.WriteLine($"Route cost: {routeCost}");
+            Car.DisplayCarCount();
+            Console.WriteLine("\r\n=========================================\r\n");
+            Garage garage1 = new Garage();
+            garage1.Address = "ul. Garażowa 1";
+            garage1.Capacity = 1;
+            Garage garage2 = new Garage("ul. Garażowa 2", 2);
+            garage1.CarEnter(car1);
+            garage1.Details();
+            garage1.CarEnter(car2);
+            garage2.CarEnter(car2);
+            var movedCar = garage1.CarLeave();
+            garage2.CarEnter(movedCar);
+            garage2.Details();
+            garage1.Details();
+            garage2.CarLeave();
+            garage2.Details();
+            garage2.CarLeave();
+            garage2.CarLeave();
+            garage2.Details();
+            garage1.Details();
+            Console.WriteLine("\r\n=========================================\r\n");
+            Person.MaxCarCount = 3;
+            Person person1 = new Person();
+            person1.FirstName = "Jan";
+            person1.LastName = "Kowalski";
+            person1.Address = "Al. NMP 1";
+            person1.AddCarRegistrationNumber("SCZ 12345");
+            person1.AddCarRegistrationNumber("SC 9863");
+            person1.AddCarRegistrationNumber("SCZ 37893");
+            person1.AddCarRegistrationNumber("SCZ 4912");
+            person1.Details();
+            Person person2 = new Person("Piotr", "Nowak", "Al. Wolnosci 10");
+            person2.AddCarRegistrationNumber("DW 9866");
+            person2.Details();
+            person2.RemoveCarRegistrationNumber("DW 9866");
+            person2.Details();
+            person2.RemoveCarRegistrationNumber("DW 9866");
+
+            Console.ReadKey();
+        }
+    }
+}
