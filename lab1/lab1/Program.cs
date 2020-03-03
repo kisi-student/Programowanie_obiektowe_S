6f46c0a3952916800802eb5c7761a531a45d2773
Bolesław B <asemeeaehe@gmail.com>
2020-03-03 09:47:51 +0100

    lab1

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class car
+    {
+        private string brand;
+        private string model;
+        private int doors;
+        private int engine;
+        private double fuel_cons;
+    }
+    public Brand
+        {
+        return brand
+}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
