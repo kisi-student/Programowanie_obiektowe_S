008afe424c0078b8dd00f806ff6e77368aa8db46
Aleksandra P <aleksandra.p@gmail.com>
2020-03-03 09:48:16 +0100

    lab1

---
+++ Lab1/Lab1/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int CapacityEngine { get; set; }
+        public double AverageCombustion { get; set; }
+
+        static int carCount=0;
+
+        public Car()
+        {
+            Brand = "none";
+            Model = "none";
+            CapacityEngine = 0;
+            AverageCombustion = 0.0;
+
+            carCount++; //przy kazdym wywolaniu konstruktora zwiekszamy licznik samochodow
+
+        }
+        public Car(string brand, string model, int capacityEngine, double averageCombustion)
+        {
+            Brand = brand;
+            Model = model;
+            CapacityEngine = capacityEngine;
+            AverageCombustion = averageCombustion;
+
+            carCount++;
+        }
+
+       private double CalculateCombustion(double lengthRoute)
+        {
+
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using zad1;
+
+namespace Lab1
+{
+    public class Program //defaultowo jest tu internal
+    {
+        static void Main(string[] args) //static --> Klasa.statcznyElement
+        {
+        }
+    }
+}
