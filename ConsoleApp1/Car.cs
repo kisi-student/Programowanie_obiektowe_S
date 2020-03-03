0b34447fb68f365c6ee78dd2b6ce4f6314099011
Łukasz <lukasz@interia.pl>
2020-03-03 09:47:45 +0100

    Lab1

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Car
+    {
+        private string _mark;
+        private string _model;
+        private int _doorCount;
+        private int _engineCapacity;
+        private double _avgFuelConsumption;
+        private static int _carsCount = 0;
+
+        public Car()
+        {
+            _mark = "unknown";
+            _model = "unknown";
+            _doorCount = 0;
+            _avgFuelConsumption = 0.0;
+            _carsCount++;
+        }
+
+        public Car(string _mark, string _model, int _doorCount, int _engineCapacity, double _avgFuelConsumption)
+        {
+            this._mark = _mark;
+            this._model = _model;
+            this._doorCount = _doorCount;
+            this._engineCapacity = _engineCapacity;
+            this._avgFuelConsumption = _avgFuelConsumption;
+            _carsCount++;
+        }
+
+        private double CalculateFuelConsumption(double _routeLength)
+        {
+            return (_avgFuelConsumption * _routeLength) / 100.0;
+        }
+
+        public double CalculateCostRide(double _routeLength, double _fuelCost)
+        {
+            return CalculateFuelConsumption(_routeLength) * _fuelCost; 
+        }
+
+        public void WriteInformation()
+        {
+            Console.WriteLine("Mark:" + _mark + "Model:" + _model + "Door Count:" + _doorCount + "Engine Capacity:" + _engineCapacity + "Avg Fuel Consumption:" + _avgFuelConsumption);
+            Console.WriteLine("/n");
+        }
+
+        public static void WriteCarsCount()
+        {
+            Console.WriteLine("Cars Count:" + _carsCount);
+            Console.WriteLine("/n");
+        }
+
+
+
+        public string Mark { get => _mark; set => _mark = value; }
+        public string Model { get => _model; set => _model = value; }
+        public int DoorCount { get => _doorCount; set => _doorCount = value; }
+        public int EngineCapacity { get => _engineCapacity; set => _engineCapacity = value; }
+        public double AvgFuelConsumption { get => _avgFuelConsumption; set => _avgFuelConsumption = value; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car s1 = new Car();
+            s1.WriteInformation();
+
+            s1.Mark = "Fiat";
+            s1.Model = "126p";
+            s1.DoorCount = 2;
+            s1.EngineCapacity = 650;
+            s1.AvgFuelConsumption = 6.0;
+
+            s1.WriteInformation();
+
+            Car s2 = new Car("Syrena", "105", 2, 800, 7.6);
+
+            s2.WriteInformation();
+
+            double kosztPrzejazdu = s2.CalculateCostRide(30.5, 4.85);
+            Console.WriteLine("Koszt przejazdu:" + kosztPrzejazdu);
+
+            Car.WriteCarsCount();
+
+            Console.ReadKey();
+        }
+    }
+}
