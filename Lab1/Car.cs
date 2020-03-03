74d62f127142343a6d61986b0167316f527e8d1d
Pawel Filipiak <fake.mail@pcz.pl>
2020-03-03 09:47:43 +0100

    Lab1: Car in progress, Garage in progress

---
+++ Lab1/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Car
+    {
+        private string marka;
+        private string model;
+        private int doorNumber;
+        private double engineCapacity;
+        private double avgPetrolUse;
+        public static int carNumber = 0;
+
+        public Car()
+        {
+            carNumber++;
+        }
+
+        public Car(string marka, string model, int doorNumber, double engineCapacity, double avgPetrolUse)
+        {
+            carNumber++;
+            this.marka = marka;
+            this.model = model;
+            this.doorNumber = doorNumber;
+            this.engineCapacity = engineCapacity;
+            this.avgPetrolUse = avgPetrolUse;
+        }
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public int DoorNumber { get => doorNumber; set => doorNumber = value; }
+        public double EngineCapacity { get => engineCapacity; set => engineCapacity = value; }
+        public double AvgPetrolUse { get => avgPetrolUse; set => avgPetrolUse = value; }
+
+        public override string ToString()
+        {
+            return $"Auto( marka:{marka}, model:{model}, l.drzwi:{doorNumber}, poj. silnika:{engineCapacity}, sr. uz. paliwa:{avgPetrolUse}";
+        }
+
+    }
+}
