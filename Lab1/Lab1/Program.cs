760ae1fd3a6845828c13d504696f5be16be37e24
Roksana <rak@onet.pl>
2020-03-03 09:48:06 +0100

    Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            public class Car
+        {
+            private string brand;
+            private string model;
+            private int doorCount;
+            private float engineVolume;
+            private float avgConsump; // averageFuelConsumption
+                                      //private string registrationNumber;
+
+
+
+
+            /*
+            public Car() // konstryktor domyslny
+                {
+                    _avgConsump = 0.0;
+                    _carCount++;
+                }
+            */
+        }
+    }
+        
+
+        // Car car1 = new Car();
+        // car1.Details();
+    }
+}
+
