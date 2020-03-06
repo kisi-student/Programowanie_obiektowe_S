c392cdd24280fc81c388c953c309723c8002bd7b
Rafal G <ra@ww.pl>
2020-03-06 17:08:04 +0100

    Lab1

---
+++ Lab1/Lab1/Garage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Proszę zauważyć ze w tym przypadku zastosowano auto-properties.
+    /// </summary>
+    public class Garage
+    {
+        public Car[] Cars { get; set; }
+        public string Address { get; set; }
+        public int CarsCount { get; set; }
+        private int _capacity;
+        public int Capacity
+        {
+            get => _capacity;
+            set
+            {
+                _capacity = value;
+                Cars = new Car[value];             
+            }
+        }
+
+        public Garage()
+        {
+            Capacity = 0;
+            Address = "none";
+            CarsCount = 0;
+        }
+
+        public Garage(string address, int capacity)
+        {
+            Capacity = capacity;
+            Address = address;
+            CarsCount = 0;
+        }
+
+        public void CarEnter(Car car)
+        {
+            if (CarsCount >= Capacity)
+                Console.WriteLine($"The garage at {Address} is full.");
+            else
+            {
+                Cars[CarsCount ] = car;
+                CarsCount++;
+            }
+        }
+
+        public Car CarLeave()
+        {
+            if (CarsCount == 0)
+            {
+                Console.WriteLine("There is no cars to leave.");
+                return null;
+            }
+            else
+            {
+                Car car = Cars[CarsCount - 1];
+                Cars[CarsCount - 1] = null;
+                CarsCount--;
+                return car;
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = $"Garage | Address: {Address}, Capacity: {Capacity}, Cars count: {CarsCount} \r\n CARS: \r\n";
+            foreach (var car in Cars)
+                str += car + "\r\n";
+            str +="+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+            return str;
+        }
+
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ Lab1/Lab1/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        public static int MaxCarCount { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int CarsCount => RegistationNumbers.Count;
+        public IList<string> RegistationNumbers { get; set; }
+
+        public Person()
+        {
+            FirstName = "none";
+            LastName = "none";
+            Address = "none";
+            RegistationNumbers = new List<string>();
+        }
+
+        public Person(string firstName, string lastName, string address)
+        {
+            RegistationNumbers = new List<string>();
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+
+        public void AddCarRegistrationNumber(string registrationNumber)
+        {
+            if (MaxCarCount <= RegistationNumbers.Count)
+                Console.WriteLine("The Person already have maximum number of cars");
+            else
+                RegistationNumbers.Add(registrationNumber);
+        }
+
+        public void RemoveCarRegistrationNumber(string registrationNumber)
+        {
+            string regNum = RegistationNumbers.FirstOrDefault(x => x.CompareTo(registrationNumber) == 0);
+            if (regNum == null)
+                Console.WriteLine($"The giver registration number is not assigned to a person.");
+            else
+                RegistationNumbers.Remove(registrationNumber);
+        }
+
+        public override string ToString()
+        {
+            string str = $"First name: {FirstName}, Last name: {LastName}, Address: {Address}, Cars count: {CarsCount} \r\n Registration numbers: ";
+            foreach (var regNum in RegistationNumbers)
+                str += regNum  +", ";
+            return str;
+        }
+        public void Details()
+        {
+            Console.WriteLine(this);
+        }
+
+    }
+}
