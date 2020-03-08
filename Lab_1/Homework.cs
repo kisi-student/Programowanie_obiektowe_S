8b687a1561567a4af156613ecd08d5b9c38a600b
Dwiecek98 <61703712+Dwiecek98@users.noreply.github.com>
2020-03-08 22:01:15 +0100

    Ukończone zadanie

---
+++ Lab_1/Homework.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars;
+using Lab_1;
+
+namespace Lab_1
+{
+    class Homework
+    {
+        public static void Run()
+        {
+            var car1 = new Car("Fiat", "126p", "SCZ6969", 2, 650, 6.0);
+            var car2 = new Car("Syrena", "105", "SCZ8888", 2, 800, 7.6);
+            var car3 = new Car("Ferrari", "Italia", "SCZ3287", 2, 800, 7.6);
+
+            var person = new Person("Ian", "Kovalski", "Melbourne, Something 6 St.");
+
+            person.PushCar(car1.RegistrationNumber);
+            person.PushCar(car2.RegistrationNumber);
+            person.PushCar(car3.RegistrationNumber);
+            person.PushCar(car3.RegistrationNumber);
+            person.PrintInfo();
+
+
+            person.PopCar(car1.RegistrationNumber);
+            person.PrintInfo();
+
+            person.PopCar(car2.RegistrationNumber);
+            person.PopCar(car3.RegistrationNumber);
+            person.PopCar(car3.RegistrationNumber);
+            person.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_1/Task_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars;
+
+namespace Task1
+{
+    class Task_1
+    {
+        public static void Run()
+        {
+            Car s1 = new Car();
+
+            s1.PrintInfo();
+
+            s1.Brand = "Fiat";
+            s1.Model = "126p";
+            s1.DoorCount = 2;
+            s1.EngineVolume = 650;
+            s1.AvarageConsump = 6.0;
+
+            s1.PrintInfo();
+
+            Car s2 = new Car("Syrena", "105","SC56Y2C", 2, 800, 7.6);
+
+            s2.PrintInfo();
+
+            double travelCost = s2.CountCostOfTravel(30.5, 4.85);
+            Console.WriteLine("Koszt przejazdu: " + travelCost);
+
+            Car.PrintNumberOfCars();
+
+            Console.ReadKey();
+        }
+    }
+}
