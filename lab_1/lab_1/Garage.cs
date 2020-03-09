a3a85ccd982e2e595263c3d61523b075be6a6c53
Daria Wiśniewska <fdfdssd@gmail.com>
2020-03-09 22:09:02 +0100

    lab_1

---
+++ lab_1/lab_1/Garage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    class Garage
+    {
+        private string address;
+        private int capacity;
+        private int number_of_cars = 0;
+        private Car[] cars;
+
+        public Garage()
+        {
+            address = "unknown";
+            capacity = 0;
+            cars = null;
+        }
+        public Garage(string _address, int _capacity)
+        {
+            address = _address;
+            capacity = _capacity;
+            cars = new Car[capacity];
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set
+            {
+                capacity = value;
+                cars = new Car[capacity];
+            }
+        }
+        public void Enter_the_car(Car car)
+        {
+            if (number_of_cars >= capacity)
+                Console.WriteLine("Garage is full");
+            else
+            {
+                cars[number_of_cars] = car;
+                number_of_cars++;
+            }
+        }
+        public Car Get_the_car_out()
+        {
+            if (number_of_cars == 0)
+            {
+                Console.WriteLine("Garage is empty");
+                return null;
+            }
+            else
+            {
+                Car car = cars[number_of_cars - 1];
+                cars[number_of_cars - 1] = null;
+                number_of_cars--;
+                return car;
+            }
+        }
+        public void Write_information()
+        {
+            Console.WriteLine("Address: {0}", address);
+            Console.WriteLine("Capacity: {0}", capacity);
+            Console.WriteLine("Number of cars: {0}", number_of_cars);
+
+            for (int i = 0; i < number_of_cars; i++)
+            {
+                cars[i].Write_info();
+            }
+        }
+    }
+}
--- lab_1/lab_1/Program.cs
+++ lab_1/lab_1/Program.cs
@@ -10,7 +10,8 @@ namespace lab_1
     {
         static void Main(string[] args)
         {
-            Car s1 = new Car();
+            // ZADANIE 1
+            /*Car s1 = new Car();
 
             s1.Write_info();
             s1.Brand = "fiat";
@@ -19,7 +20,7 @@ namespace lab_1
             s1.Engine_capacity = 650;
             s1.Average_fuel_consumption = 6.0;
 
-           s1.Write_info();
+            s1.Write_info();
 
             Car s2 = new Car("Syrena", "105", 2, 800, 7.6);
 
@@ -29,6 +30,31 @@ namespace lab_1
             Console.WriteLine("Cost of trip: {0}", cost_of_trip);
 
             Car.List_the_number_of_cars();
+            Console.ReadKey(); */
+            // ZADANIE 2
+            Car s1 = new Car("Fiat", "126p", 2, 650, 6.0);
+            Car s2 = new Car("Syrena", "105", 2, 800, 7.6);
+
+            Garage g1 = new Garage();
+            g1.Address = "ul. Garażowa 1";
+            g1.Capacity = 1;
+
+            Garage g2 = new Garage("ul. Garażowa 2", 2);
+
+            g1.Enter_the_car(s1);
+            g1.Write_information();
+            g1.Enter_the_car(s2);
+
+            g2.Enter_the_car(s2);
+            g2.Enter_the_car(s1);
+            g2.Write_information();
+
+            g2.Get_the_car_out();
+            g2.Write_information();
+
+            g2.Get_the_car_out();
+            g2.Get_the_car_out();
+
             Console.ReadKey();
 
 
