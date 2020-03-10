02411351b0cdbbb0c42bce96f2be9cc33b9e959c
Damian Bulski <damianasik@tlen.pl>
2020-03-10 09:15:22 +0100

    test2

---
+++ PO_lab/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_lab
+{
+    class Person
+    {
+        protected string name;
+        protected string surname;
+        protected string birthDate;
+
+        public Person(string name_, string surname_, string birthDate_)
+        {
+            name = name_;
+            surname = surname_;
+            birthDate = birthDate_;
+        }
+
+        public virtual void WriteInfo()
+        {
+
+        }
+    }
+}
