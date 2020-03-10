15a45e5245e1857b9423b0dba175179542eb3ed4
Michal Jura <sylkrusz@wp.p[l>
2020-03-10 09:02:43 +0100

    PO_lab2

---
+++ PO_lab/Footballer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_lab
+{
+    class Footballer: Person
+    {
+        private string position;
+        private string club;
+        private int goalCount = 0;
+
+        public Footballer(string name_, string surname_, string birthDate_, string position_, string club_) :
+            base(name_, surname_, birthDate_)
+        {
+            position = position_;
+            club = club_;
+        }
+
+        public override void WriteInfo()
+        {
+
+        }
+
+        public void ScoreGoal()
+        {
+
+        }
+    }
+}
