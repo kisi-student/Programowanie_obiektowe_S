684738a12db7718f35ce2599a4c9d454c1cb2b26
i5 <none@none.com>
2020-03-28 14:27:33 +0100

    Linked list and doubly linked list implementation

---
+++ Lab04/DoublyLinkedListEnumerable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public partial class DoublyLinkedList<T> : IEnumerable<T>
+    {
+
+        class RevEnumerator : IEnumerator<T>
+        {
+            Node firstNode, currentNode;
+
+            public RevEnumerator(Node first)
+            {
+                this.firstNode = first = currentNode = new Node { prev = first };
+            }
+
+            public T Current => this.currentNode.element;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                if (currentNode != null)
+                    currentNode = currentNode.prev;
+                return currentNode != null;
+            }
+
+            public void Reset()
+            {
+                currentNode = firstNode;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new RevEnumerator(lastElement);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new RevEnumerator(lastElement);
+        }
+    }
+}
